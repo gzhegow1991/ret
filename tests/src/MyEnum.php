c59960d6c5dfb4d90cbf9bfb6b7f3b363f50dd4a
gzhegow <6562680@gmail.com>
2026-08-25 22:28:17 +0300

    main

---
+++ tests/src/MyEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Gzhegow\Ret\Tests;
+
+enum MyEnum: string
+{
+    case ERR_FAIL = 'Error message about FAIL';
+    case ERR_FAIL_1 = 'Error message about FAIL 1';
+    case ERR_FAIL_2 = 'Error message about FAIL 2';
+}
